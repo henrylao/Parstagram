9dae559b56f6108ec8b913b4777ef9b424332220
Henry <hclao1995@gmail.com>
2021-03-20 00:27:10 -0700

    Add completion of pull to refresh

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -11,7 +11,11 @@ import Parse
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var TAG = "Feed"
     var posts = [PFObject]()
+    var numberOfPosts: Int!
+    let refreshFeed = UIRefreshControl()
+    
     @IBOutlet weak var tableView: UITableView!
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
@@ -38,14 +42,24 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // setup table view
         tableView.delegate = self
         tableView.dataSource = self
+        
+        // bind user action to a function call for loading more elements
+        refreshFeed.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.refreshControl = refreshFeed
+        self.tableView.rowHeight = UITableView.automaticDimension
+
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        loadPosts()
+    }
+    @objc func loadPosts(){
         let query = PFQuery(className: "Post")
         query.includeKey("user")
         query.limit = 20
@@ -53,11 +67,45 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         query.findObjectsInBackground{
             (posts, error) in
             if posts != nil {
+//                posts = posts! as [PFObject]
+//                self.posts.removeAll()
                 self.posts=posts!
                 self.tableView.reloadData()
+                self.refreshFeed.endRefreshing()
             }
         }
+
     }
+    
+    // endless scroller
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        if indexPath.row + 1 == posts.count{
+//            print(TAG, "loading more posts")
+////            self.loadMorePosts()
+//        }
+//    }
+    
+//
+//    func loadMorePosts(){
+//        numberOfPosts += 20
+////        let payload = [
+////            "count":numberOfTweets
+////        ]
+//        TwitterAPICaller.client?.getDictionariesRequest(url: self.timelineBaseUrl, parameters: payload, success: { (tweets : [NSDictionary]) in
+//            self.tweetArray.removeAll()
+//            for t in tweets{
+//                self.tweetArray.append(t)
+//            }
+//            self.tableView.reloadData()
+//            print(self.TAG, "successfully reloaded cell data")
+//            self.refreshTweets.endRefreshing()
+//            print(self.TAG,"successfully ended refreshing")
+//
+//        }, failure: {_  in
+//            print(self.TAG,"failed to load more tweets")
+//        })
+//
+//    }
     
 
     /*
